7cf50eb852536dda94308513ca0111ccbc22f2a8
Guilherme Bini <77159678+Bini19@users.noreply.github.com>
2021-03-08 20:27:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Código_ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t  Macoratti .net\n\tQuase tudo para Visual Basic\n");
+            char ch;
+            int i = 10;
+            do
+            {
+                Console.Write(i);
+                Console.Write(" ");
+                ch = (char)i;
+                Console.WriteLine(ch);
+                i++;
+            } while (i <= 255);
+            Console.ReadKey();
+        }
+    }
+}
